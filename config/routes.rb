a429cb69a82bc01f549fb59cf12542fd99995900
Kate-Pricer <catherine.pricer@compuware.com>
2013-02-19 06:55:05 -0500

    notworking

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 TheBash::Application.routes.draw do
   root :to => "home#index"
 
-  match '/gallery' => "home#gallery"
+  match '/home/index' => 'home#index'
+  match '/gallery' => 'home#gallery', :as => :gallery
+  match '/our_story' => 'home#our_story', :as => :our_story
+  match '/event' => 'home#event', :as => :event
+  match '/hotel' => 'home#hotel', :as => :hotel
+  match '/contact_us' => 'home#contact_us', :as => :contact_us
+  match '/about' => 'home#about', :as => :about
+  match '/gifts' => 'home#gifts', :as => :gifts
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
